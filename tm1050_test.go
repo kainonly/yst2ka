193a358e1fefaea5227037fbb75bff5ce2303599
kain <kainpro@foxmail.com>
2025-11-26 12:58:53 +0800

    test(yst2ka): add unit test for Tm1050 and refine Tm1011 test output logging
    - Added `tm1050_test.go` to cover Tm1050 functionality.
    - Refined `tm1011_test.go` by removing redundant logging and improving output readability.

--- tm1050_test.go
+++ tm1050_test.go
@@ -8,22 +8,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestYst2Ka_Tm1011(t *testing.T) {
+func TestYst2Ka_Tm1050(t *testing.T) {
 	ctx := context.TODO()
 	code := `bf10006`
 	num := Num(`X`, code, `0`)
 
-	dto := yst2ka.NewTm1011Dto(num, code,
-		`20251125101604101000402256`,
-		cfg.Phone,
-		`277733`,
+	dto := yst2ka.NewTm1050Dto(num, code, `李一四`, `1`,
+		`https://notify.kainonly.com:8443`,
 	)
-	r, err := client.Tm1011(ctx, dto)
+	r, err := client.Tm1050(ctx, dto)
 	assert.NoError(t, err)
 
 	t.Log(`code:`, r.RespCode)
 	t.Log(`msg:`, r.RespMsg)
+	t.Log(`signNum:`, r.Phone)
 	t.Log(`respTraceNum:`, r.RespTraceNum)
-	t.Log(`signNum:`, r.SignNum)
-	t.Log(`agreementNo:`, r.AgreementNo)
 }
